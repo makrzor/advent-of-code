5e3e4a25a2cc53c1e371897f5dff122ccf5fc2cc
Marcin Ornat <marcin.ornat@motorolasolutions.com>
2018-12-04 13:38:42 +0100

    AoC 2018 - testing environment added

---
+++ 2018/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#set -x
+
+for test in $(/bin/ls *.test*.txt | grep -v result | grep "^${1}"); do
+    result=$(/apps/public/bin/python ${test/.test*/.py} ${test})
+    expected=$(cat ${test/.txt/.result.txt})
+    echo -n "${test%.txt}: "
+    if [ ${result} = ${expected} ]; then
+        echo "PASS"
+    else
+        echo "FAIL [ got ${result}, expected ${expected} ]"
+    fi
+done
