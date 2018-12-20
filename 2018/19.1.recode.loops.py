112464e7f300c6bd338c186ca47976eaa589e9cf
makrzor <makrzor@gmail.com>
2018-12-20 07:49:08 +0100

    AoC 2018, day 19 - non-universal solution

---
+++ 2018/19.1.recode.loops.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+def increment_and_exit(a, c):
+    for i in range(1, c + 1):
+        for j in range(1, c + 1):
+            if c == i * j:
+                a += i
+    print(a)
+    exit(0)
+
+a = 0
+b = 114
+c = 950
+
+if a == 0 or a == 9:
+    increment_and_exit(a, c)
+if a > 9:
+    print(a)
+    exit(0)
+if a == 1:
+    b = 27
+if a < 3:
+    b *= 28
+if a < 4:
+    b += 29
+if a < 5:
+    b *= 30
+if a < 6:
+    b *= 14
+if a < 7:
+    b *= 32
+if a < 8:
+    c += b
+a = 0
+increment_and_exit(a, c)
