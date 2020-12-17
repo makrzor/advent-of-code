801d00375d6ba574da9081c2405effbfc745a486
Marcin Ornat <marcin.ornat@motorolasolutions.com>
2020-12-17 17:20:47 +0100

    AoC 2020, day 17 simplifications

--- 2020/17.1.py
+++ 2020/17.1.py
@@ -4,6 +4,8 @@ from __init__ import *
 
 GENERATIONS = 6
 
+active_cubes = 0
+
 class Cube:
 
     def __init__(self, plane, row, column):
@@ -16,81 +18,73 @@ class Cube:
         self.neighbors = []
         self.initialize_neighbors()
 
+    def add_neighbor(self, neighbor):
+        if neighbor:
+            neighbor.neighbors.append(self)
+            self.neighbors.append(neighbor)
+
     def initialize_neighbors(self):
-        for i in range (max(- GENERATIONS, self.plane - 1), self.plane + 1):
-            for j in range (max(- GENERATIONS, self.row - 1), min(rows_number - GENERATIONS, self.row + 2)):
-                for k in range(max(- GENERATIONS, self.column - 1), min(columns_number - GENERATIONS, self.column + 2)):
+        for i in range (max(0, self.plane - 1), self.plane + 1):
+            for j in range (max(0, self.row - 1), min(rows_number, self.row + 2)):
+                for k in range(max(0, self.column - 1), min(columns_number, self.column + 2)):
                     if i == self.plane and j == self.row and k == self.column:
                         return
-                    self.add_neighbor(i, j, k)
-
-    def add_neighbor(self, plane, row, column):
-        cube = grid[plane][row][column]
-        cube.neighbors.append(self)
-        self.neighbors.append(cube)
+                    self.add_neighbor(grid[i][j][k])
 
     def notify_neighbors(self, value):
         for neighbor in self.neighbors:
             neighbor.active_neighbors += value
 
+    def activate(self, change_state=0):
+        if not change_state:
+            change_state = self.change_state
+        if change_state:
+            self.active = change_state > 0
+            self.notify_neighbors(change_state)
+            global active_cubes
+            active_cubes += change_state
+            self.change_state = 0
+
 
 def initialize_grid():
-    for i in range(- GENERATIONS, planes_number - GENERATIONS):
-        for j in range(- GENERATIONS, rows_number - GENERATIONS):
-            for k in range(- GENERATIONS, columns_number - GENERATIONS):
-                grid[i][j][k] = Cube(i, j, k)
-
-
-def generation_next(active):
     for i in range(planes_number):
         for j in range(rows_number):
             for k in range(columns_number):
-                cube = grid[i][j][k]
-                if not cube.active and cube.active_neighbors == 3:
-                    cube.change_state = 1
-                elif cube.active and (cube.active_neighbors < 2 or cube.active_neighbors > 3):
-                    cube.change_state = -1
-    for i in range(planes_number):
-        for j in range(rows_number):
-            for k in range(columns_number):
-                cube = grid[i][j][k]
-                if cube.change_state == 1:
-                    cube.active = True
-                    cube.notify_neighbors(1)
-                    active += 1
-                elif cube.change_state == -1:
-                    cube.active = False
-                    cube.notify_neighbors(-1)
-                    active -= 1
-                cube.change_state = 0
-    return active
+                cube = Cube(i, j, k)
+                grid[i][j][k] = cube
+                cubes_list.append(cube)
+
+
+def generation_next():
+    for cube in cubes_list:
+        if not cube.active and cube.active_neighbors == 3:
+            cube.change_state = 1
+        elif cube.active and (cube.active_neighbors < 2 or cube.active_neighbors > 3):
+            cube.change_state = -1
+    for cube in cubes_list:
+        cube.activate()
 
 
 input_data = get_input_stream()
 initial_size = len(input_data.read().split("\n")[0])
 input_data.seek(0)
 planes_number = 1 + 2 * GENERATIONS
-rows_number = initial_size + 2 * GENERATIONS
-columns_number = initial_size + 2 * GENERATIONS
+rows_number = columns_number = initial_size + 2 * GENERATIONS
 grid = [[[None] * columns_number for j in range(rows_number)] for i in range(planes_number)]
+cubes_list = []
 initialize_grid()
-plane = 0
-row = 0
+plane = row = GENERATIONS
 
-active = 0
 for line in get_input_stream():
     line = line.strip()
-    column = 0
+    column = GENERATIONS
     for character in line:
         if character == "#":
-            cube = grid[plane][row][column]
-            cube.active = True
-            cube.notify_neighbors(1)
-            active += 1
+            grid[plane][row][column].activate(1)
         column += 1
     row += 1
 
 for i in range(GENERATIONS):
-    active = generation_next(active)
+    generation_next()
 
-print(active)
+print(active_cubes)
--- 2020/17.2.py
+++ 2020/17.2.py
@@ -4,6 +4,8 @@ from __init__ import *
 
 GENERATIONS = 6
 
+active_cubes = 0
+
 class Cube:
 
     def __init__(self, hyperplane, plane, row, column):
@@ -17,87 +19,75 @@ class Cube:
         self.neighbors = []
         self.initialize_neighbors()
 
+    def add_neighbor(self, neighbor):
+        if neighbor:
+            neighbor.neighbors.append(self)
+            self.neighbors.append(neighbor)
+
     def initialize_neighbors(self):
-        for i in range (max(- GENERATIONS, self.hyperplane - 1), self.hyperplane + 1):
-            for j in range (max(- GENERATIONS, self.plane - 1), min(planes_number - GENERATIONS, self.plane + 2)):
-                for k in range (max(- GENERATIONS, self.row - 1), min(rows_number - GENERATIONS, self.row + 2)):
-                    for l in range(max(- GENERATIONS, self.column - 1), min(columns_number - GENERATIONS, self.column + 2)):
+        for i in range (max(0, self.hyperplane - 1), self.hyperplane + 1):
+            for j in range (max(0, self.plane - 1), min(planes_number, self.plane + 2)):
+                for k in range (max(0, self.row - 1), min(rows_number, self.row + 2)):
+                    for l in range(max(0, self.column - 1), min(columns_number, self.column + 2)):
                         if i == self.hyperplane and j == self.plane and k == self.row and l == self.column:
                             return
-                        self.add_neighbor(i, j, k, l)
-
-    def add_neighbor(self, hyperplane, plane, row, column):
-        cube = grid[hyperplane][plane][row][column]
-        cube.neighbors.append(self)
-        self.neighbors.append(cube)
+                        self.add_neighbor(grid[i][j][k][l])
 
     def notify_neighbors(self, value):
         for neighbor in self.neighbors:
             neighbor.active_neighbors += value
 
+    def activate(self, change_state=0):
+        if not change_state:
+            change_state = self.change_state
+        if change_state:
+            self.active = change_state > 0
+            self.notify_neighbors(change_state)
+            global active_cubes
+            active_cubes += change_state
+            self.change_state = 0
+
 
 def initialize_grid():
-    for i in range(- GENERATIONS, hyperplanes_number - GENERATIONS):
-        for j in range(- GENERATIONS, planes_number - GENERATIONS):
-            for k in range(- GENERATIONS, rows_number - GENERATIONS):
-                for l in range(- GENERATIONS, columns_number - GENERATIONS):
-                    grid[i][j][k][l] = Cube(i, j, k, l)
-
-
-def generation_next(active):
     for i in range(hyperplanes_number):
         for j in range(planes_number):
             for k in range(rows_number):
                 for l in range(columns_number):
-                    cube = grid[i][j][k][l]
-                    if not cube.active and cube.active_neighbors == 3:
-                        cube.change_state = 1
-                    elif cube.active and (cube.active_neighbors < 2 or cube.active_neighbors > 3):
-                        cube.change_state = -1
-    for i in range(hyperplanes_number):
-        for j in range(planes_number):
-            for k in range(rows_number):
-                for l in range(columns_number):
-                    cube = grid[i][j][k][l]
-                    if cube.change_state == 1:
-                        cube.active = True
-                        cube.notify_neighbors(1)
-                        active += 1
-                    elif cube.change_state == -1:
-                        cube.active = False
-                        cube.notify_neighbors(-1)
-                        active -= 1
-                    cube.change_state = 0
-    return active
+                    cube = Cube(i, j, k, l)
+                    grid[i][j][k][l] = cube
+                    cubes_list.append(cube)
+
+
+def generation_next():
+    for cube in cubes_list:
+        if not cube.active and cube.active_neighbors == 3:
+            cube.change_state = 1
+        elif cube.active and (cube.active_neighbors < 2 or cube.active_neighbors > 3):
+            cube.change_state = -1
+    for cube in cubes_list:
+        cube.activate()
 
 
 input_data = get_input_stream()
 initial_size = len(input_data.read().split("\n")[0])
 input_data.seek(0)
-hyperplanes_number = 1 + 2 * GENERATIONS
-planes_number = 1 + 2 * GENERATIONS
-rows_number = initial_size + 2 * GENERATIONS
-columns_number = initial_size + 2 * GENERATIONS
+hyperplanes_number = planes_number = 1 + 2 * GENERATIONS
+rows_number = columns_number = initial_size + 2 * GENERATIONS
 grid = [[[[None] * columns_number for k in range(rows_number)] for j in range(planes_number)] for i in range(hyperplanes_number)]
+cubes_list = []
 initialize_grid()
-hyperplane = 0
-plane = 0
-row = 0
+hyperplane = plane = row = GENERATIONS
 
-active = 0
 for line in get_input_stream():
     line = line.strip()
-    column = 0
+    column = GENERATIONS
     for character in line:
         if character == "#":
-            cube = grid[hyperplane][plane][row][column]
-            cube.active = True
-            cube.notify_neighbors(1)
-            active += 1
+            grid[hyperplane][plane][row][column].activate(1)
         column += 1
     row += 1
 
 for i in range(GENERATIONS):
-    active = generation_next(active)
+    generation_next()
 
-print(active)
+print(active_cubes)
