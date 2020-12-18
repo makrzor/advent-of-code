86213f622adb0ef89fd28b3d3ac61e3cfe2225b4
Marcin Ornat <marcin.ornat@motorolasolutions.com>
2020-12-18 15:34:09 +0100

    AoC 2020, day 18

---
+++ 2020/18.1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re
+
+from __init__ import *
+
+
+def calculate(operation, first_operand, second_operand):
+    if operation == "+":
+        return first_operand + second_operand
+    else:
+        return first_operand * second_operand
+
+
+def simplify(data_list):
+    if len(data_list) < 3:
+        return data_list[0]
+    return simplify([calculate(data_list[1], int(data_list[0]), int(data_list[2]))] + data_list[3:])
+
+
+total_sum = 0
+for line in get_input_stream():
+    line = line.strip()
+    while "(" in line:
+        line = re.sub(r"\([^()]+\)", lambda x: str(simplify(x.group()[1:-1].split())), line)
+    total_sum += int(simplify(line.split()))
+print(total_sum)
