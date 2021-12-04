a6e0681ba48b778770c96dfd0004b5b70ba9effd
Marcin Ornat <marcin.ornat@motorolasolutions.com>
2021-12-04 14:25:06 +0100

    AoC 2021, day 4

---
+++ 2021/04.1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from __init__ import *
+
+input_stream = get_input_stream()
+queue = [int(x) for x in input_stream.readline().strip().split(",")]
+boards = []
+
+for line in input_stream:
+    if not line.strip():
+        boards.append([])
+        continue
+    boards[-1].append([])
+    for number in line.strip().split():
+        boards[-1][-1].append(int(number))
+
+for board in boards:
+    board_t = transpose(board)
+    for row in board_t:
+        board.append(row)
+
+number = -1
+winners = []
+for number in queue:
+    for board in boards:
+        for row in board:
+            if number in row:
+                row.remove(number)
+            if not row and board not in winners:
+                winners.append(board)
+    if winners:
+        break
+
+if len(winners) > 1:
+    print("More than one winner: {}".format(winners))
+board_sum = 0
+for row in winners[0]:
+    for addend in row:
+        board_sum += addend
+board_sum //= 2
+print(board_sum * number)
